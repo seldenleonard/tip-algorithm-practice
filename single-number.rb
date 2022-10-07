259f4a59be336ffe69665f685e1a915a3a7a1862
seldenleonard <selden222@gmail.com>
2022-10-07 02:58:04 -0400

    creates file and inserts leetcode link and instructions for single number problem

---
+++ single-number.rb
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/single-number/
+
+# INSTRUCTIONS
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
