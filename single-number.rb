aab5279040959c97a9dab5ea6b87a7fc3e44a050
seldenleonard <selden222@gmail.com>
2022-10-07 02:59:47 -0400

    creates preliminary method that still needs some tweaks/steps to solve single number problem

--- single-number.rb
+++ single-number.rb
@@ -9,3 +9,19 @@
 # Create an empty hash
 # Using a while loop, iterate through the array and create a hash value for each new element, and increase the value of any given key if it is already stored in the hash
 # Return the hash value that only has one instance
+
+def single_number(nums)
+  occurences = {}
+  i = 0
+  while i < nums.length
+    if occurences[nums[i]]
+      occurences[nums[i]] += 1
+    else
+      occurences[nums[i]] = 1
+    end
+    i += 1
+  end
+  if occurences[nums] == 1
+end
+
+p single_number([4,1,2,1,2])
