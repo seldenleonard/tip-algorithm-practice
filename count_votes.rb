e2a95c289bf1020de5da546b2ae419d2c33447b4
seldenleonard <selden222@gmail.com>
2022-10-06 20:14:45 -0400

    creates files for count votes problem and popular posts problem

---
+++ count_votes.rb
@@ -0,0 +1,11 @@
+# https://trello.com/c/IjfPcKsG/78-count-votes
+
+# INSTRUCTIONS
+# Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+# Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+# Output: {"Dewey" => 6, "Truman" => 5}
+
+# Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
---
+++ popular-posts.rb
@@ -0,0 +1,25 @@
+
+# INSTRUCTIONS
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+# Activity
+
+# STEPS
+# Define a method that accepts an array of hashes
+# Define an empty array
+# Using a while loop, iterate through the array, evaluating the likes within each hash
+  # Using an if statement, either enter the hash value into the empty array depending on whether it has a minimum of 1000 likes
+# Return new array (of hashes)
+
