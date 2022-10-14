085a614ece8d03eb27ab56b3ebedf33fedc7478c
seldenleonard <selden222@gmail.com>
2022-10-13 20:56:47 -0400

    sets up test for 4th prong of bowling score problem

--- bowling-game/score-bowling-game-spec.rb
+++ bowling-game/score-bowling-game-spec.rb
@@ -22,13 +22,25 @@ RSpec.describe BowlingGameScorer do
       end
     end
 
-    context "now accounting for spares, where bowler gets credit for knocking down remaining pins, AND gets double points for their next ball"
+    context "now accounting for spares, where bowler gets credit for knocking down remaining pins, AND gets the points of their next turn added to the spare"
     it "returns the score" do
       pins = [1,5,  4,4,  8,'/',  5,3,  '-','-',  4,5,  2,3,  8,'/',  7,1,  1,1]
 
       scorer = BowlingGameScorer.new(pins)
 
       if expect(scorer.total_score).to eq(78)
+        `say "CORRECT!"`
+      end
+    end
+
+
+    context "now accounting for strikes, where bowler gets credit for knocking down all pins on first try, AND gets the points of the next two turns added to the strike"
+    it "returns the score" do
+      pins = [1,5,  4,4,  8,'/',  5,3,  'X',  4,5,  2,3,  8,'/',  7,1,  1,1]
+
+      scorer = BowlingGameScorer.new(pins)
+
+      if expect(scorer.total_score).to eq(97)
         `say "CORRECT!"`
       end
     end
