d81a86dab4038154fba148f070e196a52d09fb99
seldenleonard <selden222@gmail.com>
2022-10-13 20:53:08 -0400

    completes third prong of bowling score problem

--- bowling-game/score-bowling-game-spec.rb
+++ bowling-game/score-bowling-game-spec.rb
@@ -14,7 +14,22 @@ RSpec.describe BowlingGameScorer do
 
     context "where missed throws are marked as '-'" do
       it "returns the score" do
-        
+        pins = [1,5,  4,4,  8,'-',  5,3,  '-','-',  4,5,  2,3,  8,1,  7,1,  1,1]
+
+        scorer = BowlingGameScorer.new(pins)
+
+        expect(scorer.total_score).to eq(63)
+      end
+    end
+
+    context "now accounting for spares, where bowler gets credit for knocking down remaining pins, AND gets double points for their next ball"
+    it "returns the score" do
+      pins = [1,5,  4,4,  8,'/',  5,3,  '-','-',  4,5,  2,3,  8,'/',  7,1,  1,1]
+
+      scorer = BowlingGameScorer.new(pins)
+
+      if expect(scorer.total_score).to eq(78)
+        `say "CORRECT!"`
       end
     end
   end
--- bowling-game/score-bowling-game.rb
+++ bowling-game/score-bowling-game.rb
@@ -6,6 +6,19 @@ class BowlingGameScorer
   end
 
   def total_score
-    pins.sum
+    i = 0
+    total_to_add = 0
+    while i < pins.length
+      if pins[i] == "-"
+        pins[i] = 0        
+      end
+      if pins[i] == "/"
+        pins[i] = 10 - pins[i - 1]
+        total_to_add += pins[i + 1] # or get rid of total_to_add and do: pins[i + 1] = pins[i +1] * 2
+      end
+      i += 1
+    end
+    pins.sum + total_to_add
   end
+
 end
