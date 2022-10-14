99690325af62276c2f7c2fcc10f54217667e7453
seldenleonard <selden222@gmail.com>
2022-10-13 20:20:42 -0400

    fixes error in test file

--- bowling-game/score-bowling-game-spec.rb
+++ bowling-game/score-bowling-game-spec.rb
@@ -4,7 +4,7 @@ RSpec.describe BowlingGameScorer do
   describe "#total_score" do
     context "for a 'basic game with no special or interesting throws'" do
       it "returns the score" do
-        pins = [1,5, 4,4 8,0 5,3 0,0, 4,5, 2,3, 8,1, 7,1, 1,1]
+        pins = [1,5, 4,4, 8,0, 5,3, 0,0, 4,5, 2,3, 8,1, 7,1, 1,1]
 
         scorer = BowlingGameScorer.new(pins)
 
