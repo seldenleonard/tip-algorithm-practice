ed6cb8249760e81f567875848a4bd5a04f1d2c45
seldenleonard <selden222@gmail.com>
2022-10-13 22:05:02 -0400

    wrote commented out note-to-self for how to best pick up where i left off when i return to solve algorithm

--- bowling-game/score-bowling-game.rb
+++ bowling-game/score-bowling-game.rb
@@ -10,8 +10,9 @@ class BowlingGameScorer
     total_to_add = 0
     pins_all_nums = pins
     while i < pins_all_nums.length
-      if test
-        
+      if pins_all_nums[i].string?
+        # GOAL HERE IS THIS: create a duplicate array to pins, which is pins_all_nums, and then in pins_all_nums, replace all strings (strikes, spares, and gutterballs) with integer values -- kind of like i already did below. However, in order to get around the error im currently getting (where strikes are looking ahead to values in the array to make calculations but then those values are strings), I need to not only replace all the strings with integers, but i also need to insert new 0 values in the array after each strike
+
       end
       i += 1
     end
