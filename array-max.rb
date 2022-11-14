f57faac381df91eeecab0310cb9bf1201590e3bb
seldenleonard <selden222@gmail.com>
2022-11-14 10:32:59 -0500

    creates file for array max problem and includes link to trello prompt

---
+++ array-max.rb
@@ -0,0 +1,7 @@
+# https://trello.com/c/RrVM3Fpr/116-array-max
+
+# INSTRUCTIONS
+# Write a function that returns the greatest value from an array of numbers.
+  # Input: [5, 17, -4, 20, 12]
+  # Output: 20
+
