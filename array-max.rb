9de46fd4708da6ab3727462f032ba8cafbecce3c
seldenleonard <selden222@gmail.com>
2022-11-22 09:50:55 -0500

    writes out whiteboarding steps to solve array-max.rb problem

--- array-max.rb
+++ array-max.rb
@@ -5,3 +5,11 @@
   # Input: [5, 17, -4, 20, 12]
   # Output: 20
 
+# STEPS
+# Define a method that accepts an array
+# Set a variable called "greatest_value" equal to the first value in the array
+# Define an index variable equal to 1
+# Loop through the array
+  # Compare the array[index] with greatest_value variable and if array[index] is greater than greatest_value variable, set greatest_value = array[index]
+  # Increase index by 1
+# Return greatest_value
